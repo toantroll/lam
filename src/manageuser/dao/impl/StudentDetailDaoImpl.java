c87099fb668011f773fecb726049a20483ee5eb8
hung <asd>
2017-09-21 10:58:12 +0700

    viet createStudentInfor
    StudentDetailDaoImpl

--- src/manageuser/dao/impl/StudentDetailDaoImpl.java
+++ src/manageuser/dao/impl/StudentDetailDaoImpl.java
@@ -1,8 +1,9 @@
 package manageuser.dao.impl;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import manageuser.dao.StudentDetailDao;
 import manageuser.entities.StudentDetail;
 
@@ -13,8 +14,41 @@ public class StudentDetailDaoImpl extends BaseDaoImpl implements StudentDetailDa
 	 * @see manageuser.dao.StudentDetailDao#createStudentInfor(manageuser.entities.StudentDetail)
 	 */
 	public boolean createStudentInfor(StudentDetail studentInfor) {
-		String sql = "INSERT INTO `student_detail` (`student_id`, `course_id`, `name`, `email`, `tel`, `id_card`, `address`, `school`, `major`, `graduated_year`, `gender`, `birthday`, `IQ`, `note`, `japan_level`, `interview`, `status`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
+		String sql = "INSERT INTO `student_detail` (`student_id`, `course_id`, `name`, `email`, `tel`, `id_card`, `address`, `school`, `major`, `graduated_year`, `gender`, `birthday`, `IQ`, `note`, `japan_level`, `interview`, `status`, `created_at`, `updated_at`) VALUES (LAST_INSERT_ID(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
 		PreparedStatement pre;
+		
+		int rs;
+		int i = 1;
+		try {
+			Connection con = this.getConnectionTransaction();
+			pre = con.prepareStatement(sql);
+			pre.setInt(i++, studentInfor.getCourseId());
+			pre.setString(i++, studentInfor.getName());
+			pre.setString(i++, studentInfor.getEmail());
+			pre.setString(i++, studentInfor.getTel());
+			pre.setString(i++, studentInfor.getIdCard());
+			pre.setString(i++, studentInfor.getAdress());
+			pre.setString(i++, studentInfor.getSchool());
+			pre.setString(i++, studentInfor.getMajor());
+			pre.setString(i++, studentInfor.getGraduatedYear());
+			pre.setString(i++, studentInfor.getGender());
+			pre.setDate(i++, studentInfor.getBirthday());
+			pre.setInt(i++, studentInfor.getScoreIQ());
+			pre.setString(i++, studentInfor.getNote());
+			pre.setString(i++, studentInfor.getJapanLevel());
+			pre.setInt(i++, studentInfor.getScoreInterview());
+			pre.setString(i++, studentInfor.getStatus());
+			pre.setDate(i++, studentInfor.getCreatedDate());
+			pre.setDate(i++, studentInfor.getUpdatedDate());
+			rs = pre.executeUpdate();
+			if(rs != 0 ) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Loi createStudentInfor" + e);
+		}
+		return false;
 
 	}
 }
