5c17711b5c4f5e21aa86d54f83f9b0542f9ee8f8
hung <asd>
2017-09-21 13:17:36 +0700

    update StudentDetailDao

--- src/manageuser/dao/StudentDetailDao.java
+++ src/manageuser/dao/StudentDetailDao.java
@@ -1,11 +1,19 @@
 package manageuser.dao;
+import java.sql.SQLException;
+
 import manageuser.entities.StudentDetail;
 
 public interface StudentDetailDao {
 	/**
 	 * Ghi du lieu thong tin Student vao DB
 	 * @param studentInfor đối tượng lưu thông tin
-	 * @return Ghi thành công trả về true nếu không trả về false
+	 * @throws SQLException
 	 */
-	public boolean createStudentInfor(StudentDetail studentInfor);
+	public void createStudentInfor(StudentDetail studentInfor) throws SQLException;
+	/**
+	 * Update dữ liệu thông tin StudentInfor vào Db
+	 * @param studentDetail
+	 * @throws SQLException
+	 */
+	public void updateStudentInfor(StudentDetail studentDetail) throws SQLException ;
 }
