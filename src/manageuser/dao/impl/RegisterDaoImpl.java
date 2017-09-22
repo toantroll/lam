a48a0c73db50bfd234399bbf7d85351f1d4a6e8c
bao <baobkit@gmail.com>
2017-09-22 09:13:40 +0700

    Update: RegisterDao,RegisterDaoImpl

--- src/manageuser/dao/impl/RegisterDaoImpl.java
+++ src/manageuser/dao/impl/RegisterDaoImpl.java
@@ -1,11 +1,15 @@
-/**
+﻿/**
  * 
  */
 package manageuser.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import manageuser.dao.RegisterDao;
 import manageuser.entities.Register;
@@ -14,36 +18,166 @@ import manageuser.entities.Register;
  * @author LA-PM
  *
  */
-public class RegisterDaoImpl extends BaseDaoImpl implements RegisterDao{
+public class RegisterDaoImpl extends BaseDaoImpl implements RegisterDao {
 
-	/* (non-Javadoc)
-	 * @see manageuser.dao.RegisterDao#insertRegister(manageuser.entities.Register)
+	private PreparedStatement pst;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * manageuser.dao.RegisterDao#insertRegister(manageuser.entities.Register)
 	 */
 	@Override
-	public boolean insertRegister(Register register) {
-		Connection conn = getConnection();
-		String sql = "INSERT INTO register (full_name, email, tel, graduated_year, school, major, birthday, status, created_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		try {
-			PreparedStatement ps = conn.prepareStatement(sql);
-			int i = 1;
-			ps.setString(i++, register.getFullName());
-			ps.setString(i++, register.getEmail());
-			ps.setString(i++, register.getTel());
-			ps.setInt(i++, register.getGraduatedYear());
-			ps.setString(i++, register.getSchool());
-			ps.setString(i++, register.getMajor());
-			ps.setDate(i++, register.getBirthday());
-			ps.setInt(i++, register.getStatus());
-			ps.setDate(i++, register.getCreatedDate());
-			ps.execute();
-			ps.close();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Lỗi câu lệnh sql " + e.getMessage());
-			return false;
-		}finally {
-			closeConnection();
+	public boolean addUserRegist(Register regiter) {
+		Connection con = getConnection();
+		if (con != null) {
+			StringBuffer sql = new StringBuffer();
+			int i = 0;
+			sql.append(
+					"insert into register (full_name,email,birthday,tel,school,graduated_year,major,status,created_date,iq) ")
+					.append("values (?,?,?,?,?,?,?,?,?,?)");
+			System.out.println(sql.toString());
+			try {
+				pst = con.prepareStatement(sql.toString());
+			
+				pst.setString(++i, regiter.getFullName());
+				pst.setString(++i, regiter.getEmail());
+				pst.setDate(++i, regiter.getBirthday());
+				pst.setString(++i, regiter.getTel());
+				pst.setString(++i, regiter.getSchool());
+				pst.setInt(++i, regiter.getGraduatedYear());
+				pst.setString(++i, regiter.getMajor());
+				pst.setInt(++i, regiter.getStatus());
+				pst.setDate(++i, regiter.getCreatedDate());
+				pst.setInt(++i, regiter.getIq());
+				return (pst.executeUpdate() == 1);
+			} catch (SQLException e) {
+				System.out.println(e);
+			} finally {
+				closeConnection();
+			}
+
 		}
+		return false;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see manageuser.dao.RegisterDao#deleteUserRegist(int id)
+	 */
+	@Override
+	public boolean deleteUserRegist(int id) {
+		Connection con = getConnection();
+		if (con != null) {
+			StringBuffer sql = new StringBuffer();
+			int i = 0;
+			sql.append("delete from register where id =? ");
+			try {
+				pst = con.prepareStatement(sql.toString());
+				pst.setInt(++i, id);
+				return (pst.executeUpdate() == 1);
+			} catch (SQLException e) {
+				// logger.log(null, e.getMessage());
+			} finally {
+				closeConnection();
+			}
+
+		}
+
+		return false;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * manageuser.dao.RegisterDao#updateUserRegist(manageuser.entities.Register)
+	 */
+	@Override
+	public boolean updateUserRegist(Register register) {
+		Connection con = getConnection();
+		if (con != null) {
+			StringBuffer sql = new StringBuffer();
+			int i = 0;
+			sql.append(
+					"update register set  full_name=? , email=? , birthday=?, tel=? , school=? , graduated_year=? ,major=?,status=?,created_date=?,iq=?")
+					.append(" where id=? ");
+			try {
+				pst = con.prepareStatement(sql.toString());
+				pst.setString(++i, register.getFullName());
+				pst.setString(++i, register.getEmail());
+				pst.setDate(++i, register.getBirthday());
+				pst.setString(++i, register.getTel());
+				pst.setString(++i, register.getSchool());
+				pst.setInt(++i, register.getGraduatedYear());
+				pst.setString(++i, register.getMajor());
+				pst.setInt(++i, register.getStatus());
+				pst.setDate(++i, register.getCreatedDate());
+				pst.setInt(++i, register.getIq());
+				pst.setInt(++i, register.getId());
+
+				return (pst.executeUpdate() == 1);
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+				// logger.log(null, e.getMessage());
+			} finally {
+				closeConnection();
+			}
+
+		}
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see manageuser.dao.RegisterDao#addListUserRegist(List<Register>)
+	 */
+	@Override
+	public boolean addListUserRegist(List<Register> listRegister) {
+		Connection con = getConnection();
+		if (getConnection() != null) {
+			StringBuffer sql = new StringBuffer().append("Insert into register (full_name, email, birthday, tel, school, graduated_year,major,status,created_date,iq) values ");
+			for (Register register : listRegister) {
+				sql.append("(?,?,?,?,?,?,?,?,?,?),");
+			}
+
+			sql.deleteCharAt(sql.length()-1);
+			System.out.println(sql.toString());
+			int i = 0;
+			try {
+				pst = con.prepareStatement(sql.toString());
+				for (Register register : listRegister) {
+
+					pst.setString(++i, register.getFullName());
+					pst.setString(++i, register.getEmail());
+					pst.setDate(++i, register.getBirthday());
+					pst.setString(++i, register.getTel());
+					pst.setString(++i, register.getSchool());
+					pst.setInt(++i, register.getGraduatedYear());
+					pst.setString(++i, register.getMajor());
+					pst.setInt(++i, register.getStatus());
+					pst.setDate(++i, register.getCreatedDate());
+					pst.setInt(++i, register.getIq());
+				}
+				
+				return (pst.executeUpdate() > 0);
+
+			} catch (SQLException e) {
+				System.out.println(e);
+			} finally {
+				closeConnection();
+			}
+		}
+		return false;
+	}
+
+	 
 }
+
+
+
