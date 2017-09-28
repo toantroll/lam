b59765f61811e93f96c0aa0eb4f6684d0c29d7dc
hopdv <dvhopptit@gmail.com>
2017-09-28 09:32:56 +0700

    update CourseDao

--- src/manageuser/dao/CourseDao.java
+++ src/manageuser/dao/CourseDao.java
@@ -5,6 +5,7 @@
  */
 package manageuser.dao;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import manageuser.entities.Course;
@@ -15,14 +16,30 @@ import manageuser.entities.Course;
  */
 public interface CourseDao {
 	/**
-	 * lấy danh sách khóa học 
-	 * @return danh sách khoa học. không thể null; 
+	 * lấy danh sách khóa học
+	 * 
+	 * @return danh sách khoa học. không thể null;
 	 */
-		public ArrayList<Course> getListCourse();
-		/**
-		 * kiểm tra khóa học có tồn tại hay không 
-		 * @param courseID mã khóa học cần kiểm tra 
-		 * @return true nếu tồn tại khóa học và false nếu không tồn tại   
-		 */
-		public boolean existCourse(int courseID);
+	public ArrayList<Course> getListCourse();
+
+	/**
+	 * kiểm tra khóa học có tồn tại hay không
+	 * 
+	 * @param courseID
+	 *            mã khóa học cần kiểm tra
+	 * @return true nếu tồn tại khóa học và false nếu không tồn tại
+	 */
+	public boolean existCourse(int courseID);
+
+	/**
+	 * check exists
+	 * 
+	 * @param id
+	 *            course id
+	 * @return return true if exists
+	 * @throws SQLException
+	 *             SQLException
+	 */
+	public boolean isExistCourseById(int id) throws SQLException;
+
 }
